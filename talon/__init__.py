bd989da527103b844038f37200802a7cc98044da
Alexander Shereshevsky <shereshevsky@gmail.com>
2020-09-16 20:45:11 +0300

    bruteforcer only

--- talon/__init__.py
+++ talon/__init__.py
@@ -1,13 +1,13 @@
 from __future__ import absolute_import
 from talon.quotations import register_xpath_extensions
-try:
-    from talon import signature
-    ML_ENABLED = True
-except ImportError:
-    ML_ENABLED = False
+# try:
+#     from talon import signature
+#     ML_ENABLED = True
+# except ImportError:
+#     ML_ENABLED = False
 
 
 def init():
     register_xpath_extensions()
-    if ML_ENABLED:
-        signature.initialize()
+    # if ML_ENABLED:
+    #     signature.initialize()
